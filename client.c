d22585b726f723e890df868d53e72339856699f3
Yuvan Rengifo <104460732+YuvanRen@users.noreply.github.com>
2024-04-18 22:18:05 -0400

    Add files via upload
    Usage = completed.
    Server set up = completed.
    Connection_Handling = completed.
    Game logic = In Progress.
                        - Prints question to screen
                        - Checks for mid game disconnections
                                 - Determines how many active players and if game should continue
                         - Determines winner and closes all fds

---
+++ client.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <errno.h>
+/*
+    Yuvan Rengifo  
+    I pledge my honor that I have abided by the Stevens Honor System.
+
+*/
+#define RED "\x1B[38;5;196m" 
+#define DEFAULT "\x1b[0m"
+#define YELLOW "\x1B[38;5;227m"
+
+#define DEFAULT_IP "127.0.0.1"
+#define DEFAULT_PORT 25555
+
+
+void print_usage(char *prog) {
+    printf("Usage: %s [-i IP_address] [-p port_number] [-h]\n", prog);
+    printf(" -i IP_address          Default to \"127.0.0.1\";\n");
+    printf(" -p port_number         Default to 25555;\n");
+    printf(" -h                     Display this help info.\n");
+}
+
+void parse_connect(int argc, char** argv, int* server_fd) {
+
+    int opt;
+    char *ip_address = DEFAULT_IP;
+    int port_number = DEFAULT_PORT;
+    
+    /* First argument must be an opt if one passed*/
+    if (argc > 1 && argv[1][0] != '-') {
+        fprintf(stderr,"%sUnknown option '%s' received. %s\n", RED,argv[1],DEFAULT);
+        exit(EXIT_FAILURE);
+    }
+
+    opterr = 0; /* Not printing built in error*/
+    while ((opt = getopt(argc, argv, "hi:p:")) != -1) {
+        switch (opt) {
+            case 'h':
+                print_usage(argv[0]);
+                exit(EXIT_SUCCESS);
+            case 'i':
+                ip_address = optarg;
+                break;
+            case 'p':
+                port_number = atoi(optarg);
+                break;
+            case '?':
+                if (optopt == 'i' || optopt == 'p') {
+                    fprintf(stderr, "%sError: Option '-%c' requires an argument.%s\n", RED, optopt, DEFAULT);
+                } else {
+                    fprintf(stderr, "%sUnknown option '-%c' received%s.\n", RED, optopt, DEFAULT);
+                }
+                exit(EXIT_FAILURE);
+        }
+    }
+/* Creating and Setting up Socket */
+    struct sockaddr_in server_addr;
+
+    if((*server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+        perror("Error Creating Socket");
+        exit(1);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port_number);
+
+    if (inet_pton(AF_INET, ip_address, &server_addr.sin_addr) <= 0) {
+        perror("Invalid address");
+        exit(1);
+    }
+/* Try to connect to server*/
+    if (connect(*server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection Failed");
+        exit(1);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int server_fd;
+    parse_connect(argc, argv, &server_fd);
+    char buffer[1024];
+    while(1) {
+        /* Receive messages from server*/
+        int recvbytes = recv(server_fd, buffer, 1024, 0);
+        if (recvbytes == 0) break;
+        else {
+            buffer[recvbytes] = 0;
+            printf("%s%s%s", YELLOW ,buffer,DEFAULT); fflush(stdout);
+        }
+        /* Send messages to server*/
+        scanf("%s", buffer);
+        send(server_fd, buffer, strlen(buffer), 0);
+
+    }
+
+    close(server_fd);  
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,410 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <errno.h>
+
+/*
+    Yuvan Rengifo  
+    I pledge my honor that I have abided by the Stevens Honor System.
+
+*/
+
+#define DEFAULT_FILE "question.txt"
+#define DEFAULT_IP "127.0.0.1"
+#define DEFAULT_PORT 25555
+#define MAX_CONNECTIONS 2
+
+#define YELLOW "\x1B[38;5;227m"
+#define RED "\x1B[38;5;196m"  /* For errors */
+#define BLUE "\x1B[1;34m"
+#define GREEN "\x1B[32m"
+#define DEFAULT "\x1b[0m"
+
+void print_usage(char *prog) {
+    printf("Usage: %s [-f question_file] [-i IP_address] [-p port_number] [-h]\n\n", prog);
+    printf(" -f question_file       Default to \"question.txt\";\n");
+    printf(" -i IP_address          Default to \"127.0.0.1\";\n");
+    printf(" -p port_number         Default to 25555;\n");
+    printf(" -h                     Display this help info.\n");
+}
+
+struct Entry{
+    char prompt[1024];
+    char options[3][50];
+    int answer_idx;
+}Entry;
+
+struct Player{
+    int fd;
+    int score;
+    char name[128];
+}Player;
+
+int read_questions(struct Entry* arr, char* filename){
+    int count = 0;  /* Questions count*/
+    FILE* fp = fopen(filename, "r");
+
+    /* Failed to open file */
+    if (!fp) {
+        fprintf(stderr, "%sError: Can't open file. %s\n", RED, DEFAULT);
+        exit(EXIT_FAILURE);
+    }
+
+    char line[1024];
+
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        /* Skip any empty lines */ 
+        if (strcmp(line, "\n") == 0) continue;
+
+        /* Copy the question prompt */ 
+        strcpy(arr[count].prompt, line);
+
+        /* Read the options line */ 
+        if (fgets(line, sizeof(line), fp) == NULL) break;
+        sscanf(line, "%s %s %s", arr[count].options[0], arr[count].options[1], arr[count].options[2]);
+
+        /* Read the answer index line */ 
+        if (fgets(line, sizeof(line), fp) == NULL) break;
+        arr[count].answer_idx = atoi(line);
+
+        /* Increment question count */
+        count++;
+
+        /* Too many questions*/
+        if (count >= 50) break;
+    }
+
+    fclose(fp);
+    return count; 
+}
+
+/* Helper to display messages to players mid game*/
+void display(struct Player* players, int total_players, const char *message) {
+    for (int i = 0; i < total_players; i++) {
+        if (players[i].fd != 0) {
+            write(players[i].fd, message, strlen(message));
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    char *question_file = DEFAULT_FILE;
+    char *ip_address = DEFAULT_IP;
+    int port_number = DEFAULT_PORT;
+    int opt;
+
+    /* Parsing Arguments*/
+
+    /* First argument must be an opt*/
+    if (argc > 1 && argv[1][0] != '-') {
+        fprintf(stderr,"%sUnknown option '%s' received. %s\n", RED,argv[1],DEFAULT);
+        exit(EXIT_FAILURE);
+    }
+
+    opterr = 0; /* Not printing built in error*/
+    while ((opt = getopt(argc, argv, "hf:i:p:")) != -1) {
+        switch (opt) {
+
+            case 'h':
+                print_usage(argv[0]);
+                return EXIT_SUCCESS;
+
+            case 'f':
+                question_file = optarg;
+                break;
+
+            case 'i':
+                ip_address = optarg;
+                break;
+
+            case 'p':
+                port_number = atoi(optarg);
+                break;
+
+            case '?':
+                if (optopt == 'f' || optopt == 'i' || optopt == 'p')  {
+                    fprintf(stderr,"%sError: Option '-%c' requires an argument.%s\n",RED, optopt, DEFAULT);
+                } else{
+                    fprintf(stderr,"%sUnknown option '-%c' received%s.\n",RED, optopt, DEFAULT);
+                }
+                exit(EXIT_FAILURE);
+                }
+        }
+
+    /* IPC stuff setup*/    
+    int server_fd;
+    int client_fd;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in in_addr;
+    socklen_t addrlen = sizeof(in_addr);
+
+/* Creating and Setting up Socket */
+    if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+        perror("Error Creating Socket");
+        exit(1);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family        = AF_INET;
+    server_addr.sin_port          = htons(port_number);
+
+    /* Make sure ip is valid */
+    if (inet_pton(AF_INET, ip_address, &server_addr.sin_addr) <= 0) {
+        perror("Invalid address");
+        exit(1);
+    }
+
+/* Bind file descriptor with adress structure*/
+    if(bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr))){
+                perror("Error binding");
+                exit(1);
+            }
+    
+ /* Listen at most 2 connections*/
+    if(listen(server_fd, MAX_CONNECTIONS) != 0){
+        perror("Listening error");
+        exit(1);
+    }
+
+/* Succeeds :D */
+    char* Indent = "                ";
+    printf("%s%s+------------------------+\n", YELLOW,Indent);
+    printf("%s%s| Welcome to 392 Trivia! |\n",YELLOW,Indent);
+    printf("%s%s+------------------------+%s\n", YELLOW,Indent, DEFAULT);
+
+
+/* Read questions database*/
+    struct Entry questions[50]; /* Max 50 questions */
+    int quesNum = read_questions(questions, question_file); /* get number of questions*/
+    if (quesNum < 0) {
+        fprintf(stderr, "%sFailed to read questions from the file%s\n",RED, DEFAULT);
+        exit(EXIT_FAILURE);
+    }
+
+/* Accepting connections to enable comms*/
+    fd_set myset;
+    FD_ZERO(&myset);
+    FD_SET(server_fd,&myset);
+    struct Player players[MAX_CONNECTIONS];
+    int player_c = 0;
+    int game_started = 0;
+    int max_fd = server_fd;
+
+    /* Inititate every players attributes*/
+    for(int i = 0; i < MAX_CONNECTIONS; i ++) {
+        players[i].fd = -1;
+        strcpy(players[i].name, "null");
+        players[i].score =0;
+    }
+
+    char buffer[1024];
+    int recvbytes = 0;
+    char* getName = "Please type your name:\n";
+
+
+    /* To accept connections*/
+    while(1){
+
+        if (game_started) {
+            break;  /* Game already started*/
+        }
+
+        /* Re-initiation */
+        FD_SET(server_fd,&myset);
+        max_fd = server_fd;
+
+        for(int i = 0; i< MAX_CONNECTIONS; i++){
+            if(players[i].fd != -1 ){ 
+                FD_SET(players[i].fd,&myset);
+                if(players[i].fd > max_fd) max_fd = players[i].fd;
+            }
+        }
+
+        /* Monitor fds*/
+        select(max_fd+1,&myset,NULL, NULL, NULL);
+
+        /* Incoming connection */
+        if(FD_ISSET(server_fd, &myset)){
+            client_fd = accept(server_fd,
+                                (struct sockaddr*)&in_addr,
+                                &addrlen);
+            if(player_c < MAX_CONNECTIONS){
+                printf("%sNew connection detected!%s\n",GREEN,DEFAULT);
+                player_c++;
+                for(int i = 0; i < MAX_CONNECTIONS; i++){
+                    if(players[i].fd == -1){
+                        players[i].fd = client_fd;
+                        write(players[i].fd, getName, strlen(getName));
+                        break;
+                    } 
+
+                }
+            }else{
+                printf("%sMax connections reached!%s\n",RED,DEFAULT);
+                close(client_fd);
+            }
+        }
+        /* Check which player is ready*/
+        for(int i =0; i< MAX_CONNECTIONS;i++){
+            if(players[i].fd != -1 && FD_ISSET(players[i].fd, &myset)){
+                 /* Request players name*/
+                    /* read name */
+                        /* && */
+               /* Check for lost connection*/
+                recvbytes = read(players[i].fd, buffer, 1024);
+                if(recvbytes == 0){
+                    printf("%sConnection lost!%s\n",RED,DEFAULT);
+                    close(players[i].fd);
+                    players[i].fd = -1;
+                    player_c --;
+
+                }else{
+                buffer[recvbytes] = 0; /* Null terminate*/
+                printf("Hi %s!\n", buffer);
+
+                /* Set players score and name*/
+                strcpy(players[i].name,buffer);
+                }
+            }
+        }
+
+        /* All players are ready to begin game*/
+        int flag = 1;
+        for (int i = 0; i < MAX_CONNECTIONS; i++) {
+            /* Check if any player has still "null" as the name */ 
+            if (strcmp(players[i].name, "null") == 0) {
+                flag = 0; /* Not all players have a name */
+                break;
+            }
+        }
+        if (player_c == MAX_CONNECTIONS && flag == 1 ){
+            printf("%s%s  THE GAME STARTS NOW! :D%s\n\n", BLUE, Indent,DEFAULT);
+            game_started = 1; /* Get out of accepting loop*/
+        } 
+    } 
+
+/* Start Game logic */
+int quesIndx = 0;
+
+    while(1){
+
+/* Go through each question*/
+    if( quesIndx < quesNum){
+
+    /* Question and Options to screen */
+        printf("%sQuestion %d: %s%s", YELLOW, quesIndx + 1, questions[quesIndx].prompt,DEFAULT);
+        printf("1: %s\n2: %s\n3: %s\n\n", questions[quesIndx].options[0], 
+                                        questions[quesIndx].options[1], 
+                                        questions[quesIndx].options[2]);
+    /* Question and Option to players*/
+
+
+/* Read answers and check for any lost connection*/
+
+        fd_set read_fds;
+        FD_ZERO(&read_fds);
+        int max_fd = 0;
+
+        for (int i = 0; i < MAX_CONNECTIONS; i++) {
+            if (players[i].fd != -1) {
+                FD_SET(players[i].fd, &read_fds);
+                if (players[i].fd > max_fd) {
+                    max_fd = players[i].fd;
+                }
+            }
+        }
+
+        select(max_fd + 1, &read_fds, NULL, NULL,NULL);
+
+/* Players ready for IO*/
+        for(int i = 0; i < MAX_CONNECTIONS; i++){
+            if(players[i].fd != -1 && FD_ISSET(players[i].fd, &read_fds)){
+
+                char answer[2];
+
+                int ans_bytes = read(players[i].fd, answer, 2);
+
+            /* Disconnected player handler*/
+                if(ans_bytes == 0){
+                    printf("%sConnection lost!%s\n",RED,DEFAULT);
+                    close(players[i].fd);
+                    players[i].fd = -1;
+
+            /* Check for current active players */
+                    int active_players = 0;
+                    int remaining_player=0;
+                        for (int j = 0; j < MAX_CONNECTIONS; j++) {
+                             if (players[j].fd != -1) {
+                                active_players++;
+                                remaining_player = j;
+                                 }
+                            }
+            /* Forfeit winning message*/
+                        if(active_players == 1){
+                            printf("%s%s Wins by forfeit!%s\n\n",GREEN, players[remaining_player].name,DEFAULT);
+                            close(players[remaining_player].fd);
+                            quesIndx = quesNum;
+                            break;
+                        }
+            /* All players disconnected*/
+                        else if(active players == 0){
+                            printf("%sGame end, no more players in the server :(%s\n\n", RED, DEFAULT);
+                            quesIndx = quesNum;
+                            break;
+                        }
+
+            /* Check who gets answer correct first and assign points */
+                }else{
+                    answer[ans_bytes] = 0; /* Terminator*/
+                    int id = atoi(answer) - 1;
+
+                    if(id == questions[quesNum].answer_idx){
+                        players[i].score++;
+            /* Send out correct answer */
+                        display(players, MAX_CONNECTIONS, /*correct message*/ );
+                        break;
+
+                    }
+
+                }
+
+
+            }
+        }
+        quesIndx++; /* Next question*/
+
+        }else{
+
+            break; /* Went through all the questions*/
+        }
+}
+
+/* Determine Winner*/
+    int max = 0;
+    int winner_idx = 0;
+    for (int i = 0; i < MAX_CONNECTIONS; i++) {
+        if (players[i].fd > 0 && players[i].score > max) {
+            max = players[i].score;
+            winner_idx = i;
+        }
+    }
+
+/* Winner message*/             
+    printf("%sCongrats, %s!%s\n", GREEN,players[winner_idx].name,DEFAULT);  // Print winner on server's screen
+
+/* Close all connections*/
+    for (int i = 0; i < MAX_CONNECTIONS; i++) {
+        if (players[i].fd != 0) {
+            close(players[i].fd);
+            players[i].fd = 0;
+        }
+    }
+    close(server_fd);
+    return 0;
+}
